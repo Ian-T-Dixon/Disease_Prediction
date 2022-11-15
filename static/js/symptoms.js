36643f19fe0db189c26a269eb653d8e2cc7965a4
Ian Dixon <iantdixon@gmail.com>
2022-11-15 15:07:50 -0500

    updated symptoms.js

--- static/js/symptoms.js
+++ static/js/symptoms.js
@@ -1,21 +1,47 @@
 function init() {
-  // Grab a reference to the dropdown select element
+  // Grab a reference to the symtpom list
   var selector = d3.select("#symptomList");
 
-  // Use the list of sample names to populate the select options
+  // Use the list of symptoms to populate the list of checkboxes
   d3.json("./static/data/symptoms.json").then((data) => {
     var symptomNames = data.ID;
-    // console.log(symptomNames);
+    symptomArray = Object.values(symptomNames);
 
-    Array.from(symptomNames).forEach((symptom) => {
-      console.log(symptom)
+    
+
+    symptomArray.forEach(symptom => {
       selector
-      .append("option")
-      .text(symptom)
-      .property("value", symptom);
+        .append("h6")
+      selector
+        .append("input")
+        .property("type", "checkbox")
+        .property("value", symptom)
+        .property("id", symptom);
+      selector
+        .append("label")
+        .property("for", symptom)
+        .text(symptom)
+    });
   });
-  })
 };
+
+
 
 // Initialize the dashboard
 init();
+
+// Create the output array from SymptomList
+function createSymptomArray() {
+  var checkboxes = document.getElementById("symptomList");
+  var outputArray = []
+  for (var i = 0; i < checkboxes.length; i++) {
+    var checkbox = checkboxes[i];
+    if (checkbox.checked) {
+      outputArray.push(1);
+  }
+    else {
+      outputArray.push(0);
+    }
+  }
+  console.log(outputArray);
+}
