b85520fc3932ba528a82bc1d9b807d26b61b7c8d
Ian Dixon <iantdixon@gmail.com>
2022-11-17 13:21:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, jsonify
+import pickle
+import json
 
 app = Flask(__name__)
-
+model = pickle.load(open('./static/data/svm_model.pkl', 'rb'))
 
 @app.route('/')
-def hello():
+def home():
     return render_template('index.html')
+
+@app.route('/output', methods=['POST'])
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    #reads checked box data from form and displays as JSON.
+    symptoms = request.form.getlist("symptom")
+    return jsonify(symptoms)
+    
+
+    
+    
+if __name__ == "__main__":
+    app.run(debug=True)
