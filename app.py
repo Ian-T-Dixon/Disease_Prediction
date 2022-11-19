10c679597abb26c3c1d53c20363601bfd5adbc28
Ian Dixon <iantdixon@gmail.com>
2022-11-19 13:26:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, request, render_template, jsonify
 import numpy as np
 import pickle
 import json
-
+from sklearn.metrics import (accuracy_score,
+    confusion_matrix, ConfusionMatrixDisplay)
+    
 app = Flask(__name__)
 model = pickle.load(open('./static/data/svm_model.pkl', 'rb'))
 
@@ -10,14 +12,11 @@ model = pickle.load(open('./static/data/svm_model.pkl', 'rb'))
 def home():
     return render_template('index.html')
 
-@app.route('/predict', methods=['GET','POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    #reads checked box data from form and displays as JSON.
-    # symptoms = request.form.getlist("symptom")
-    # return jsonify(symptoms)
+    # requests data from POST and imports the symptom boolean dictionary produced from the form data.
     if request.method == "POST":
         bool_dict = request.get_json()
-        print(bool_dict)
         
         list_features = [True if bool(bool_dict[key]) else False
         # Iterate through symptoms alphabetically
@@ -26,11 +25,8 @@ def predict():
 
         # Pass boolean array to model for prediction
         prediction = model.predict(array_features)[0]
-
-        return render_template('index.html',
-        prediction_text='These symptoms appear to be caused by {}'.format(prediction)
-        )
-
+        print(prediction)
+        return render_template('index.html', prediction_text="These symptoms appear to be caused by {}".format(prediction))
     
     
 if __name__ == "__main__":
