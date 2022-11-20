6b492b54996184d3eb7ab7f172994ed8c0bc0c92
e-pluribus-unum <42368816+e-pluribus-datum@users.noreply.github.com>
2022-11-20 18:20:35 -0500

    fix form submission response

--- app.py
+++ app.py
@@ -1,37 +1,55 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template
 import numpy as np
 import pickle
-import json
+import csv
 
 app = Flask(__name__)
-model = pickle.load(open('./static/data/svm_model.pkl', 'rb'))
 
-@app.route('/')
+# Unpickle trained ML model
+# SWITCH back to svm_model after fixing 'acidity' symptom
+model = pickle.load(open('./static/data/rfc_model.pkl', 'rb'))
+
+# Read all possible symptoms into stored list
+# TODO: Update this file with new symptom names
+# May connect to SQL database instead for symptom names
+with open('./static/data/symptoms_alpha.csv', newline='') as f:
+    reader = csv.reader(f)
+    symptom_list = sorted(
+        [x[0] for x in reader][1:]      # [1:] ignores header
+    )
+
+# Save loaded symptoms from .csv or SQL query as json file
+# Have Flask do this on startup to ensure compatability
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
-
-@app.route('/predict', methods=['GET','POST'])
-def predict():
-    #reads checked box data from form and displays as JSON.
-    # symptoms = request.form.getlist("symptom")
-    # return jsonify(symptoms)
     if request.method == "POST":
-        bool_dict = request.get_json(force=True)
-        print(bool_dict)
-        
+        # Receive dictionary of checked symptom names and Boolean values
+        # Only True values are sent by form submission
+        bool_dict = request.form.to_dict()
+
+        # Fill in False values for the unchecked symptoms
+        # symptoms.json should match the source of symptom_list
+        for symptom in symptom_list:
+            if symptom not in bool_dict:
+                bool_dict[symptom] = False
+
+        # List of T/F for alphabetically sorted symptoms
         list_features = [True if bool(bool_dict[key]) else False
-        # Iterate through symptoms alphabetically
-        for key in sorted(bool_dict)]
+                         for key in sorted(bool_dict)]
         array_features = [np.array(list_features)]
 
         # Pass boolean array to model for prediction
         prediction = model.predict(array_features)[0]
 
         return render_template('index.html',
-        prediction_text='These symptoms appear to be caused by {}'.format(prediction)
-        )
+			prediction_text='These symptoms appear to be caused by {}.'
+				.format(prediction),
+			# Any additional parameters to be updated on index.html
+		)
 
-    
-    
+    else:
+        return render_template('index.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/symptoms.js
+++ static/js/symptoms.js
@@ -1,88 +1,30 @@
 function init() {
-  // Grab a reference to the symtpom list
+  // Grab a reference to the symptom list
   var selector = d3.select("#symptomList");
 
   // Use the list of symptoms to populate the list of checkboxes
   d3.json("../static/data/symptoms.json").then((data) => {
     var symptomNames = data.ID;
     symptomArray = Object.values(symptomNames);
-
     symptomArray.forEach(symptom => {
       selector
         .append("h6")
       selector
         .append("input")
         .property("type", "checkbox")
-        .property("value", symptom)
-        .property("id", symptom)
-        .property("name", "symptom")
+        .property("value", "True")    // defaults to 'on'
+        .property("id", symptom)      // symptom
+        .property("name", symptom)    // key
       selector
         .append("label")
-        .property("for", symptom)
+        .property("for", symptom)     // must match 'id'
         .text(symptom)
     });
   });
 };
 
-
-
 // Initialize the dashboard
 init();
 
-// Create the output array from SymptomList
-function createSymptomArray() {
-  var checkboxes = document.getElementById("symptomList");
-  var outputArray = []
-  var outputDict = {}
-  
-  // Loop through checkboxes and create a dictionary with true/false values.
-  for (var i = 0; i < checkboxes.length; i++) {
-    var checkbox = checkboxes[i];
-    if (checkbox.checked) {
-      outputDict[checkbox.value] = true
-  }
-    else {
-      outputDict[checkbox.value] = false
-    }
-  }
-    delete outputDict.Submit
-    console.log(outputDict)
-// sends json filetype to flask
-    fetch('/predict', {
-      headers : {
-          'Content-Type' : 'application/json'
-      },
-      method : 'POST',
-      body : JSON.stringify(outputDict)
-  })
-  .then(function (response){
-
-      if(response.ok) {
-          response.json()
-          .then(function(response) {
-              console.log(response);
-          });
-      }
-      else {
-          throw Error('Something went wrong');
-      }
-  })
-  .catch(function(error) {
-      console.log(error);
-  });
-}
-  //// working code for creating an array with true/flse values.  
-  // for (var i = 0; i < checkboxes.length; i++) {
-  //   var checkbox = checkboxes[i];
-  //   if (checkbox.checked) {
-  //     outputArray.push(true);
-  // }
-  //   else {
-  //     outputArray.push(false);
-  //   }
-  // }
-  // for (var g = 0; g < outputArray.length; g++) {
-  //   var symptom = outputArray[g];
-  //   outputDict[symptom] = g
-  // }
-// console.log(outputArray)
+// Function to clear all checkboxes
+// Called when "Clear All" button pressed
